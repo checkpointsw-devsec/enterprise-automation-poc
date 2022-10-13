1f576fcca57dd7fad2a6ea110a427ac175380685
Jim Öqvist <59479436+chkp-jimo@users.noreply.github.com>
2022-10-13 15:35:40 +0200

    Update variables.tf

--- terraform/2.1-resilient-security-gateway/variables.tf
+++ terraform/2.1-resilient-security-gateway/variables.tf
@@ -51,12 +51,3 @@ variable = "chkp_otp_key" {
 variable = "chkp_admin_password_plain" {
   type = string
 } 
-
-# This password hash is for vpn123 - please do not use this for production
-chkp_passwd_hash =  "\\$6\\$UAxWXDhef2zm8mqt\\$gvnwQIF1dkw8J9JUvmyYXUeAt2LSl4V62JKNFilEK0YQGo4ACOTry4QLWjE9nMQOPCKs42ZleldciXgCdnjCd1"
-
-# Default SIC key - please also replace this with your key
-chkp_otp_key = "vpn123"
-
-# Default admin password - plain text option. To use a hash - paste the hash value here and change the value in deploy-resilient-gateways.tf for password-type to Hash
-chkp_admin_password_plain = "Cpwins12345!"
