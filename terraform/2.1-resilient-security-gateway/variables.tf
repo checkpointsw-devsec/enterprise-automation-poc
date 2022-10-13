a169681b0c9f4c8b89a1ce247e78534f43d49d76
Stuart Green [Check Point] <71756633+chkp-stuartgreen@users.noreply.github.com>
2022-10-13 12:30:04 +0100

    Update variables.tf

--- terraform/2.1-resilient-security-gateway/variables.tf
+++ terraform/2.1-resilient-security-gateway/variables.tf
@@ -39,3 +39,24 @@ variable "vsphere_folder_name" {
   type = string
   default = "automation-poc"
 }
+
+variable = "chkp_passwd_hash" {
+  type = string
+}
+
+variable = "chkp_otp_key" {
+  type = string
+}
+
+variable = "chkp_admin_password_plain" {
+  type = string
+} 
+
+# This password hash is for vpn123 - please do not use this for production
+chkp_passwd_hash =  "\\$6\\$UAxWXDhef2zm8mqt\\$gvnwQIF1dkw8J9JUvmyYXUeAt2LSl4V62JKNFilEK0YQGo4ACOTry4QLWjE9nMQOPCKs42ZleldciXgCdnjCd1"
+
+# Default SIC key - please also replace this with your key
+chkp_otp_key = "vpn123"
+
+# Default admin password - plain text option. To use a hash - paste the hash value here and change the value in deploy-resilient-gateways.tf for password-type to Hash
+chkp_admin_password_plain = "Cpwins12345!"
